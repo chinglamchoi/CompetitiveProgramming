11d07c0be4d0c96de88c326075de0074bd9eca92
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-17 00:49:59 +0800

    Update D309.pas

--- D309.pas
+++ D309.pas
@@ -6,7 +6,7 @@ begin
 readln(S);
 readln(T);
 for i := 1 to length(s) do if ord(s[i]) > 96 then s[i] := chr(ord(s[i])-32);
-for i := 1 to length(t) do if ord(s[i]) > 96 then s[i] := chr(ord(s[i])-32);
+for i := 1 to length(t) do if ord(t[i]) > 96 then s[i] := chr(ord(t[i])-32);
 
 if s = t then writeln('Equal') else
   if s < T then writeln('Smaller') else writeln('Greater');
