c7df10e5f3fa0dc06d638a1bfa3599995cdfd53b
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-17 00:49:41 +0800

    Create D309.pas

---
+++ D309.pas
@@ -0,0 +1,14 @@
+program D309;
+
+var S, T :string[100];  i : integer;
+
+begin
+readln(S);
+readln(T);
+for i := 1 to length(s) do if ord(s[i]) > 96 then s[i] := chr(ord(s[i])-32);
+for i := 1 to length(t) do if ord(s[i]) > 96 then s[i] := chr(ord(s[i])-32);
+
+if s = t then writeln('Equal') else
+  if s < T then writeln('Smaller') else writeln('Greater');
+  readln;
+end.
