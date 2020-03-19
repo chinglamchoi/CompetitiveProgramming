95cac0550f2c5bf93934f3827d251a1b4c119477
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-20 00:50:08 +0800

    Create D702.py

---
+++ D702.py
@@ -0,0 +1,22 @@
+array1 = []
+input1 = int(input())
+input2 = None
+for i in range(input1):
+    input2 = input()
+    if len(input2) == 4: #SIZE
+        print(len(array1))
+    elif len(input2) == 3:
+        if input2 == "POP": #POP
+            if len(array1) == 0:
+                print("Cannot pop")
+            else:
+                array1 = [array1.pop(len(array1) - 1)]
+        elif input2 == "TOP": #TOP
+            if len(array1) == 0:
+                print("Empty")
+            else:
+                print(array1[0])
+    else: #PUSH
+        a, b = input2.split()
+        b = int(b)
+        array1.insert(0, b)
