ebb2bb642c2e0ebf6a626404132ebdde4365b410
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-20 00:50:48 +0800

    Update D701.py

--- D701.py
+++ D701.py
@@ -3,19 +3,19 @@ input1 = int(input())
 input2 = None
 for i in range(input1):
     input2 = input()
-    if len(input2) == 4:
+    if len(input2) == 4: #size
         print(len(array1))
-    elif len(input2) == 5:
+    elif len(input2) == 5: #front
         if len(array1) == 0:
             print("Empty")
         else:
-            print(array1[len(array1) - 1])
-    elif len(input2) == 3:
+            print(array1[len(array1)-1])
+    elif len(input2) == 3: #pop
         if len(array1) == 0:
             print("Cannot pop")
         else:
-            array1 = [array1.pop(0)]
-    else:
+            array1.pop(len(array1)-1)
+    else: #PUSH
         a, b = input2.split()
         b = int(b)
         array1.insert(0, b)
