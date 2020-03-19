f56478067dab797958264f833b2500069da1bd24
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-20 00:49:06 +0800

    Create D701.py

---
+++ D701.py
@@ -0,0 +1,21 @@
+array1 = []
+input1 = int(input())
+input2 = None
+for i in range(input1):
+    input2 = input()
+    if len(input2) == 4:
+        print(len(array1))
+    elif len(input2) == 5:
+        if len(array1) == 0:
+            print("Empty")
+        else:
+            print(array1[0])
+    elif len(input2) == 3:
+        if len(array1) == 0:
+            print("Cannot pop")
+        else:
+            array1 = [array1.pop(len(array1)-1)]
+    else:
+        a, b = input2.split()
+        b = int(b)
+        array1.append(b)
