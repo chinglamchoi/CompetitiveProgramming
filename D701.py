fe40e4f1f21730311a44380a23bf113971ca3ead
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-20 00:49:21 +0800

    Update D701.py

--- D701.py
+++ D701.py
@@ -9,13 +9,13 @@ for i in range(input1):
         if len(array1) == 0:
             print("Empty")
         else:
-            print(array1[0])
+            print(array1[len(array1)-1])
     elif len(input2) == 3:
         if len(array1) == 0:
             print("Cannot pop")
         else:
-            array1 = [array1.pop(len(array1)-1)]
+            array1 = [array1.pop(0)]
     else:
         a, b = input2.split()
         b = int(b)
-        array1.append(b)
+        array1.insert(0, b)
