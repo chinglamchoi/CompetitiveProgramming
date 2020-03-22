37456436deccd5ff61e3606f77a0c6d916cea425
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-22 10:25:25 +0800

    Create D309.py

---
+++ D309.py
@@ -0,0 +1,26 @@
+count = 0
+a = list(input().lower())
+b = list(input().lower())
+a = ["i","n","f","o","r","m","a","t","i","o","n"]
+b = ["i","n","f","o","r","m","a","t","i","c","s"]
+if len(a) > len(b):
+    print("Greater")
+elif len(a) < len(b):
+    print("Smaller")
+else:
+    for i in a:
+        a[count] = ord(a[count])
+        count = count + 1
+    count = 0
+    for k in b:
+        b[count] = ord(b[count])
+        count = count + 1
+    count = 0
+    while a[count] == b[count] and count < len(a):
+        count = count + 1
+    if a[count] < b[count]:
+        print("Smaller")
+    elif a[count] > b[count]:
+        print("Greater")
+    else:
+        print("Equal")
