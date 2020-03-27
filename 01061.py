42952dc21f6d4dd1f89675fced0f7483ce6ca685
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-28 00:42:11 +0800

    Create 01061.py

---
+++ 01061.py
@@ -0,0 +1,12 @@
+a = input()
+count = 0
+if a.count(",") != 5:
+    print("Invalid")
+else:
+    b = a.split(",")
+    a = 0
+    for i in b:
+        if b[count] == " " or b[count] == "":
+            a = a + 1
+        count = count + 1
+    print(a)
