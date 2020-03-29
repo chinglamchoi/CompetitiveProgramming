a7c922f95dacb00ff76a76badfcdde48a79ad436
Ching Lam Choi <ccl5a09@gmail.com>
2020-03-29 12:14:22 +0800

    Update M17A2.py

--- M17A2.py
+++ M17A2.py
@@ -1,8 +1,14 @@
 c = 0
 count = 0
+e = [0]
 a = int(input())
 b = input().split(" ")
 for i in b:
-    c = c + int(b[count])
+    if e.count(int(b[count])) == 0:
+        e.append(int(b[count]))
+    count = count + 1
+count = 0
+for k in e:
+    c = c + e[count]
     count = count + 1
 print(c)
