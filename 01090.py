3ea781ea70970614a1cca1d8ef203e3537912f3d
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-01 00:27:33 +0800

    Update 01090.py

--- 01090.py
+++ 01090.py
@@ -2,8 +2,9 @@ a = int(input())
 b = []
 for i in range(a):
     c = input()
-    b.append(c)
-    if b.count(c) % 2 == 0:
+    if c in b:
+        b.remove(c)
         print("out")
     else:
         print("in")
+        b.append(c)
