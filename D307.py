4d5fc966c24fd277c708670858a0eb71768e7529
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-02 00:49:35 +0800

    Update D307.py

--- D307.py
+++ D307.py
@@ -4,7 +4,7 @@ for i in range(3):
     a = input()
     for i in a:
         b.append(a[cnt])
-        cnt = cnt + 1
+        cnt += 1
     cnt = 0
 if b.count("X") < 3 and b.count("O") < 3:
     print("Not ended")
@@ -21,5 +21,4 @@ else:
         elif (b[0] == b[1] == b[2] == "O") or (b[3] == b[4] == b[5] == "O") or (b[6] == b[7] == b[8] == "O") or (b[0] == b[3] == b[6] == "O") or (b[1] == b[4] == b[7] == "O") or (b[2] == b[5] == b[8] == "O") or (b[0] == b[4] == b[8] == "O") or (b[2] == b[4] == b[6] == "O"):
             print("O wins")
         else:
-            while True:
-                cnt = cnt + 1
+            print("Not ended")
