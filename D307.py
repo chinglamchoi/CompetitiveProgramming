d6aef1165875ddc6216ad3cc7ceb1e2d31512036
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-02 00:49:15 +0800

    Update D307.py

--- D307.py
+++ D307.py
@@ -20,3 +20,6 @@ else:
             print("X wins")
         elif (b[0] == b[1] == b[2] == "O") or (b[3] == b[4] == b[5] == "O") or (b[6] == b[7] == b[8] == "O") or (b[0] == b[3] == b[6] == "O") or (b[1] == b[4] == b[7] == "O") or (b[2] == b[5] == b[8] == "O") or (b[0] == b[4] == b[8] == "O") or (b[2] == b[4] == b[6] == "O"):
             print("O wins")
+        else:
+            while True:
+                cnt = cnt + 1
