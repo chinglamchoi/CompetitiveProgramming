c90693ae609fc3a86f8b8dec2c498d0641856de4
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-02 00:48:51 +0800

    Update D307.py

--- D307.py
+++ D307.py
@@ -9,9 +9,12 @@ for i in range(3):
 if b.count("X") < 3 and b.count("O") < 3:
     print("Not ended")
 else:
-    if len(b) == 9 and not((b[0] == b[1] == b[2]) or (b[3] == b[4] == b[5]) or (b[6] == b[7] == b[8]) or (b[0] == b[3] == b[6])
+    if not((b[0] == b[1] == b[2]) or (b[3] == b[4] == b[5]) or (b[6] == b[7] == b[8]) or (b[0] == b[3] == b[6])
                                  or (b[1] == b[4] == b[7]) or (b[2] == b[5] == b[8]) or (b[0] == b[4] == b[8]) or (b[2] == b[4] == b[6])):
-        print("Draw")
+        if len(b) == 9:
+            print("Draw")
+        else:
+            print("Not ended")
     else:
         if (b[0] == b[1] == b[2] == "X") or (b[3] == b[4] == b[5] == "X") or (b[6] == b[7] == b[8] == "X") or (b[0] == b[3] == b[6] == "X") or (b[1] == b[4] == b[7] == "X") or (b[2] == b[5] == b[8] == "X") or (b[0] == b[4] == b[8] == "X") or (b[2] == b[4] == b[6] == "X"):
             print("X wins")
