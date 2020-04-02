9d1bb6dff20e46c240916d7b073618a8ee16a97f
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-03 00:05:43 +0800

    Update M0521.py

--- M0521.py
+++ M0521.py
@@ -1,11 +1,11 @@
 a = input()
-a = a.strip(" ")
+a = a.replace(" ", "")
 e = 0
-
 def st(a):
     global e
     eg = ["23456789"]
     a = list(a)
+    a.sort()
     if ((''.join(a) in eg) or (''.join(a) == "2345A") or (''.join(a) == "6789T") or (''.join(a) == "AJKQT") or (''.join(a) == "89JQT") or (''.join(a) == "789JT") or (''.join(a) == "9JKQT")):
         e = e + 0.1
 
@@ -24,10 +24,10 @@ def fof(a):
 if (a.count("D") == 5) or (a.count("C") == 5) or (a.count("H") == 5) or (a.count("S") == 5):
     e = 0.3
 a = ''.join(a)
-a = a.strip("D")
-a = a.strip("C")
-a = a.strip("H")
-a = a.strip("S")
+a = a.replace("D", "")
+a = a.replace("C", "")
+a = a.replace("H", "")
+a = a.replace("S", "")
 st(a)
 fof(a)
 if e == 0:
