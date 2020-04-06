4addf95f3b65ef53fef14ea0a51d76a75121a4d1
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-07 00:03:30 +0800

    Create D107.py

---
+++ D107.py
@@ -0,0 +1,23 @@
+import math
+a = int(input())
+
+def is_int(s):
+    if s % 1 == 0:
+        return True
+    else:
+        return False
+    
+
+if (is_int(math.sqrt(a)) == True) and (is_int(math.sqrt(8*a+1)) == True): 
+    print("Both")
+elif (is_int(math.sqrt(a)) == True) and (is_int(math.sqrt(8*a+1)) == False):
+    print("Square")
+elif (is_int(math.sqrt(8*a+1)) == True) and (is_int(math.sqrt(a)) == False):
+    print("Triangular")
+else:
+    print("Neither")
+
+#Doesn't work for second to last case
+
+
+
