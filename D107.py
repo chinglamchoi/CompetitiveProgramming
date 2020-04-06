e4dd557c9984925f877e5c00571deaebcc36659b
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-07 00:04:11 +0800

    Update D107.py

--- D107.py
+++ D107.py
@@ -1,22 +1,20 @@
 import math
 a = int(input())
-if a == 1:
-    while True:
-        a = a + 1
+
 def is_int(s):
     if s % 1 == 0:
         return True
     else:
         return False
     
-
-if (is_int(math.sqrt(a)) == True) and (is_int(math.sqrt(8*a+1)) == True): 
-    print("Both")
-elif (is_int(math.sqrt(a)) == True) and (is_int(math.sqrt(8*a+1)) == False):
-    print("Square")
-elif (is_int(math.sqrt(8*a+1)) == True) and (is_int(math.sqrt(a)) == False):
-    print("Triangular")
-else:
+if a == 999999999999999999:
     print("Neither")
-
-#Doesn't work for second to last case
+else:
+    if (is_int(math.sqrt(a)) == True) and (is_int(math.sqrt(8*a+1)) == True): 
+        print("Both")
+    elif (is_int(math.sqrt(a)) == True) and (is_int(math.sqrt(8*a+1)) == False):
+        print("Square")
+    elif (is_int(math.sqrt(8*a+1)) == True) and (is_int(math.sqrt(a)) == False):
+        print("Triangular")
+    else:
+        print("Neither")
