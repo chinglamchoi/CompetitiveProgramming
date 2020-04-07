a549febb201c47ec3f7164b31256678bfe7b7710
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-08 00:18:23 +0800

    Update M1801.py

--- M1801.py
+++ M1801.py
@@ -1,26 +1,16 @@
 a = input().split(" ")
 b = input().split(" ")
+if ("9" in a) and ("6" not in a):
+    a.append("6")
+elif ("6" in a) and ("9" not in a):
+    a.append("9")
+if ("9" in b) and ("6" not in b):
+    b.append("6")
+elif ("6" in b) and ("9" not in b):
+    b.append("9")
 for i in range(int(input())):
-    c = list(input())
-    if (c[0] in a) and (c[1] in b):
-        print("Yes")
-    elif (c[0] in b) and (c[1] in a):
+    c = input()
+    if ((c[0] in a) and (c[1] in b)) or ((c[0] in b) and (c[1] in a)):
         print("Yes")
     else:
-        if ((c[0] == "9") and ("6" in a)) or ((c[0] == "6") and ("9" in a)):
-            if c[1] in b:
-                print("Yes")
-            elif ((c[1] == "9") and ("6" in b)) or ((c[1] == "6") and ("9" in b)):
-                print("Yes")
-            else:
-                print("No")
-        elif ((c[1] == "9") and ("6" in a)) or ((c[0] == "6") and ("9" in a)):
-            if c[0] in b:
-                print("Yes")
-            elif ((c[0] == "9") and ("6" in b)) or ((c[1] == "6") and ("9" in b)):
-                print("Yes")
-            else:
-                print("No")
-        else:
-            print("No")
-    
+        print("No")
