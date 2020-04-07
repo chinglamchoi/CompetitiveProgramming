0ff1435915f073ef48dc77e0769fcef5def9d791
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-08 00:15:40 +0800

    Update M17A5.py

--- M17A5.py
+++ M17A5.py
@@ -2,9 +2,9 @@ a = int(input())
 b = input().split(" ")
 cnt = 0
 for i in b:
+    if (cnt == len(b) -1) and (int(b[-1]) % 2== 0):
+        print("Goodest English")
     if int(b[cnt]) % 2 != 0:
         print(b[cnt])
         break
     cnt = cnt + 1
-if (cnt == len(b)-1) and (int(b[-1]) % 2 == 0):
-    print("Goodest English")
