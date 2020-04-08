d4e3f7e2e9a22b6c34257a4970c438507380719f
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-09 00:30:54 +0800

    Update J163.py

--- J163.py
+++ J163.py
@@ -1,20 +1,11 @@
 a = int(input())
 b = input().split(" ")
-c = int(input())
+input()
 d = input().split(" ")
+b = [int(i) for i in b]
+b = sum(b)
 for i in range(0, len(d)):
-    count = 0
-    for j in range(0, len(b)):
-        if b[j] != d[i]:
-            if int(b[j]) > int(d[i]):
-                count = count + int(b[j]) - int(d[i])
-            else:
-                count = count + int(d[i]) - int(b[j])
-    if count == 0:
+    if int(b - int(d[i]) * a) % 2 == 0:
         print("Robo")
     else:
-        if count % 2 == 0:
-            print("Robo")
-        else:
-            print("Alice")
-        
+        print("Alice")
