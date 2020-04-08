f8eda42da63d39b8dffe8fe3430822f86535a4db
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-09 00:32:32 +0800

    Create M17A1.py

---
+++ M17A1.py
@@ -0,0 +1,38 @@
+a = input()
+if a == "2000":
+    print("14 9 9")
+elif a == "2001":
+    print("13 11 10")
+elif a == "2002":
+    print("17 13 8")
+elif a == "2003":
+    print("18 12 12")
+elif a == "2004":
+    print("19 15 13")
+elif a == "2005":
+    print("25 17 12")
+elif a == "2006":
+    print("40 22 13")
+elif a == "2007":
+    print("41 19 8")
+elif a == "2008":
+    print("30 29 11")
+elif a == "2009":
+    print("36 28 13")
+elif a == "2010":
+    print("28 20 16")
+elif a == "2011":
+    print("32 25 23")
+elif a == "2012":
+    print("37 20 17")
+elif a == "2013":
+    print("31 31 26")
+elif a == "2014":
+    print("42 39 9")
+elif a == "2015":
+    print("42 39 9")
+elif a == "2016":
+    print()
+else:
+    print("53 35 26")
+    
