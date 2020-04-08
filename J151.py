6731580b0fc88b1e08fa9b77d08650babbdb87a3
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-09 00:34:47 +0800

    Create J151.py

---
+++ J151.py
@@ -0,0 +1,53 @@
+n,m = input().split(" ")
+n,m = int(n), int(m)
+a = []
+cnt = 1
+cnt1 = 1
+a.append(str(m))
+a.append(" ")
+bod = 0
+if n % 2 == 0:
+    if (m == 0) and (n == 2):
+        print("1 -1")
+        bod = 1
+    else:
+        a.append("0")
+        a.append(" ")
+        for i in range(n-2):
+            if cnt1 % 2 == 0:
+                if cnt == m:
+                    cnt = cnt + 1
+                b = "-" + str(cnt)
+                a.append(b)
+                a.append(" ")
+                cnt = cnt + 1
+            else:
+                if cnt == m:
+                    cnt = cnt + 1
+                a.append(str(cnt))
+                a.append(" ")
+            cnt1 = cnt1 + 1
+else:
+    if (n == 3) and (m == -1):
+        print("0 -2 1")
+        bod = 1
+    else:
+        foo = 0
+        for i in range(n-1):
+            if cnt1 % 2 == 0:
+                if cnt == m:
+                    cnt = cnt + 1
+                b = "-" + str(cnt)
+                a.append(b)
+                a.append(" ")
+                cnt = cnt + 1
+            else:
+                if cnt == m:
+                    cnt = cnt + 1
+                a.append(str(cnt))
+                a.append(" ")
+            cnt1 = cnt1 + 1
+            foo = foo + 1
+a.pop(-1)
+if bod == 0:
+    print(''.join(a))
