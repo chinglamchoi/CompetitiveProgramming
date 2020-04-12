933f0e0d2ae938d771108e5933f14b7e992100f8
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-13 00:33:15 +0800

    Update D104.py

--- D104.py
+++ D104.py
@@ -1,13 +1,14 @@
 import math as m
 
 a,b,c = input().split(" ")
-a,b,c = int(x), int(y), int(z)
+a,b,c = int(a), int(b), int(c)
 
 d = b**2-4*a*c
 if d < 0:
     print("None")
 elif d == 0:
     d = format(((-b+m.sqrt(b**2-4*a*c))/2*a), ".3f")
+    print(d)
 else:
     d = format(((-b+m.sqrt(b**2-4*a*c))/2*a), ".3f")
     e = format(((-b-m.sqrt(b**2-4*a*c))/2*a), ".3f")
