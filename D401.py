b57a35bf76d82db649dd52d266cb74f86141362f
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-17 00:13:03 +0800

    Update D401.py

--- D401.py
+++ D401.py
@@ -1,19 +1,19 @@
 a = int(input())
-b = [True]*(a+1)
 
-#b[0] and b[1] should be ignored
+b = [True]*(a+1)
+# #b[0] and b[1] should be ignored
 c = int(a**(.5))+1
-for i in range(2, c):
-	if b[i]:
-		count = 0
-		cnt = 0
-		while cnt < a:
-			cnt = i**2+count*i
-			if cnt > a:
-				break
-			else:
-				b[cnt] = False
-				count = count + 1 
-for i in range(2, len(b)):
+for i in range(2, a+1):
+	if i <= c:
+		if b[i]:
+			count = 0
+			cnt = 0
+			while cnt < a:
+				cnt = i**2+count*i
+				if cnt > a:
+					break
+				else:
+					b[cnt] = False
+					count = count + 1 
 	if b[i]:
 		print(i)
