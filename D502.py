369ec4fed4585cb90dd844ea45ff842fd9a1fd12
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-20 00:26:02 +0800

    Update D502.py

--- D502.py
+++ D502.py
@@ -1,27 +1,22 @@
 e = []
 cnt = 0
 d = [str(i) for i in range(0, 10)]
-try:
-	with open("weather.txt") as file:
-		for j in file:
-			b = list(j)
-			f = "".join(b)
-			if len(b) > 1:
-				cnt = -11
-				again = True
-				c = str()
-				if ("degrees" not in f) or ("." in f):
-					cnt = -10
-				while again:
-					if b[cnt] not in d:
-						again = False
-						break
-					if b[cnt] in d:
-						c = b[cnt] + c
-						cnt = cnt - 1
-				e.append(int(c))
-		print(str(min(e)) + " " + str(max(e)))
-	file.close()
-except FileNotFoundError:
-	while True:
-		e = 1
+with open("weather.txt") as file:
+	for j in file:
+		b = list(j)
+		f = "".join(b)
+		if len(b) > 1:
+			cnt = -11
+			again = True
+			c = str()
+			if ("degrees" not in f):
+				cnt = -10
+			while again:
+				if b[cnt] not in d:
+					again = False
+					break
+				if b[cnt] in d:
+					c = b[cnt] + c
+					cnt = cnt - 1
+			e.append(int(c))
+	print(str(min(e)) + " " + str(max(e)))
