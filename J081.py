543921413f661e0c26774e310433ddf5cc594acb
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-20 00:27:00 +0800

    Create J081.py

---
+++ J081.py
@@ -0,0 +1,11 @@
+J5i9n9 = int(input())
+MRVY1a = set()
+for LFT9RM in range(J5i9n9):
+	QnQwc6 = input()
+	Fx16aL = QnQwc6[0]
+	MRVY1a.add(Fx16aL)
+	exec(QnQwc6)
+MRVY1a = sorted(list(MRVY1a))
+
+for LFT9RM in range(len(MRVY1a)):
+	exec("print(MRVY1a[LFT9RM]+'='+str(eval(MRVY1a[LFT9RM])))")
