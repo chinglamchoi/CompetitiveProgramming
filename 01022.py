a609ffd8e801aeb9973299ea8a24f956f7151aa3
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-24 08:03:43 +0800

    Update 01022.py

--- 01022.py
+++ 01022.py
@@ -1,26 +1,10 @@
 a = int(input())
-b = {}
-c = set()
+rank = dict()
 
-d = input()
-e = int(input())
-c.add(e)
-b[e] = d
+for i in range(a):
+	b = input()
+	rank[b] = int(input())
 
-for i in range(a-1):
-	d = input()
-	e = int(input())
-	z = len(c)
-	c.add(e)
-	if len(c) != z:
-		b[e] = d
-	else:
-		b[e] = b[e] + " " + d
-b = sorted(b.items(), reverse=True)
-for i in b:
-	if " " not in i[1]:
-		print(i[1] + " " + str(i[0]))
-	else:
-		z = sorted(i[1].split(" ")) #consider using .sort()
-		for j in z:
-			print(j + " " + str(i[0]))
+rank = sorted(rank.items(), key=lambda x: (-x[1], x[0]))
+for i in range(a):
+	print(rank[i][0] + " " + str(rank[i][1]))
