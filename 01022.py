0cd60b91558e191fe05374c898238bea2585f3e9
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-24 08:02:46 +0800

    Update 01022.py

--- 01022.py
+++ 01022.py
@@ -1,7 +1,11 @@
-import collections
 a = int(input())
+rank = dict()
+
 for i in range(a):
-    input()
-    input()
-while True:
-    a = a + 1
+	b = input()
+	rank[b] = int(input())
+
+rank = sorted(rank.items(), key=lambda x: (x[1], x[0]), reverse=True)
+for i in range(a):
+	print(rank[i][0] + " " + str(rank[i][1]))
+	# print("".join(str(rank[i])))
