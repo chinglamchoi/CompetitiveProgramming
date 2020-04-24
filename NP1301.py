b767f9719726422da1c50e367473ec4959401029
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-24 08:00:36 +0800

    Update NP1301.py

--- NP1301.py
+++ NP1301.py
@@ -1,5 +1,6 @@
 cnt = 0
 with open("count.in", "r") as f:
+	global cnt
 	a,b = f.readline().split(" ")
 	a = int(a)
 	for i in range(1, (a+1)):
@@ -7,4 +8,5 @@ with open("count.in", "r") as f:
 f.close()
 
 with open("count.out", "w") as f:
+	global cnt
 	f.write(str(cnt))
