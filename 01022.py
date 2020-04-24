e45ae7679075f5d9c1b5fe5a9f7bd1b795b38f73
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-24 08:03:24 +0800

    Update 01022.py

--- 01022.py
+++ 01022.py
@@ -3,13 +3,13 @@ b = {}
 c = set()
 
 d = input()
-e = input()
+e = int(input())
 c.add(e)
 b[e] = d
 
 for i in range(a-1):
 	d = input()
-	e = input()
+	e = int(input())
 	z = len(c)
 	c.add(e)
 	if len(c) != z:
@@ -19,8 +19,8 @@ for i in range(a-1):
 b = sorted(b.items(), reverse=True)
 for i in b:
 	if " " not in i[1]:
-		print(i[1] + " " + i[0])
+		print(i[1] + " " + str(i[0]))
 	else:
 		z = sorted(i[1].split(" ")) #consider using .sort()
 		for j in z:
-			print(j + " " + i[0])
+			print(j + " " + str(i[0]))
