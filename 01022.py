5d9cca1150f77a43c2cf55f0b00d30fe46d30466
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-24 08:03:09 +0800

    Update 01022.py

--- 01022.py
+++ 01022.py
@@ -1,11 +1,26 @@
 a = int(input())
-rank = dict()
+b = {}
+c = set()
 
-for i in range(a):
-	b = input()
-	rank[b] = int(input())
+d = input()
+e = input()
+c.add(e)
+b[e] = d
 
-rank = sorted(rank.items(), key=lambda x: (x[1], x[0]), reverse=True)
-for i in range(a):
-	print(rank[i][0] + " " + str(rank[i][1]))
-	# print("".join(str(rank[i])))
+for i in range(a-1):
+	d = input()
+	e = input()
+	z = len(c)
+	c.add(e)
+	if len(c) != z:
+		b[e] = d
+	else:
+		b[e] = b[e] + " " + d
+b = sorted(b.items(), reverse=True)
+for i in b:
+	if " " not in i[1]:
+		print(i[1] + " " + i[0])
+	else:
+		z = sorted(i[1].split(" ")) #consider using .sort()
+		for j in z:
+			print(j + " " + i[0])
