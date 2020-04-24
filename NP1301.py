139299f9b601ca8e60fc65aee57cc56583d6a5e5
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-24 08:00:04 +0800

    Update NP1301.py

--- NP1301.py
+++ NP1301.py
@@ -1,14 +1,9 @@
-"""
-If TLE: do calculations
-"""
-
-
 with open("count.in", "r") as f:
 	cnt = 0
 	a,b = f.readline().split(" ")
 	a = int(a)
-	for i in range(1, a+1):
-		cnt = cnt + str(i).count(b)
+	for i in range(1, (a+1)):
+		cnt = cnt + (str(i).count(b))
 f.close()
 
 with open("count.out", "w") as f:
