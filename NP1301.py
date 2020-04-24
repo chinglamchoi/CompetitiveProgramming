24d0e77d7b3ef4acbe3be4d05ed0ecb55af8ae6f
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-24 08:01:12 +0800

    Update NP1301.py

--- NP1301.py
+++ NP1301.py
@@ -1,12 +1,12 @@
 cnt = 0
-with open("count.in", "r") as f:
-	global cnt
+with open("count.in.txt", "r") as f:
 	a,b = f.readline().split(" ")
+	if "\n" in b:
+		b = b[:(len(b)-3)]
 	a = int(a)
 	for i in range(1, (a+1)):
 		cnt = cnt + (str(i).count(b))
 f.close()
 
-with open("count.out", "w") as f:
-	global cnt
+with open("count.out.txt", "w") as f:
 	f.write(str(cnt))
