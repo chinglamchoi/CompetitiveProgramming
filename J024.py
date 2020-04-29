dddf20281e3f69ddf9c5cf12d3f719322b6383ea
Ching Lam Choi <ccl5a09@gmail.com>
2020-04-30 01:01:55 +0800

    Update J024.py

--- J024.py
+++ J024.py
@@ -1,22 +1,50 @@
-import statistics
-
 a = int(input())
-b = []
+b = {}
+c = set()
+s = 0
+y = []
+
 for i in range(a):
-	b.append(int(input()))
+	d = int(input())
+	y.append(d)
+	s += d
+	z = len(c)
+	c.add(d)
+	if len(c) == z:
+		#repeated
+		b[d] += 1
+	else:
+		b[d] = 1
+		#max(b.keys.values and output the key)
 
-q = statistics.mean(b)
-w = statistics.median(b)
-e = statistics.mode(b)
-
-m = str(q)
+#mean
+s = s/a
+m = str(s)
 if "." not in m:
-	print(format(q, ".3f"))
+	s = format(s, ".3f")
+	#check s' vlaue + type
 else:
 	i, o = m.split(".")
 	if (len(o) > 3) and (o[3] == "5"):
-		q = str(i) + "." + o[:2] + str(int(o[2])+1)
+		s = str(i) + "." + o[:2] + str(int(o[2])+1)
 	else:
-		q = format(q, ".3f")
-print(w)
-print(e)
+		s = format(s, ".3f")
+print(s)
+
+#median
+y.sort()
+if a % 2 == 0:
+	y = (y[int(a/2)] + y[int(a/2)-1])/2
+	r = str(y)
+	t, u = r.split(".")
+	if u != "0":
+		print(y)
+	else:
+		print(int(y))
+else:
+	y = y[int(a/2)]
+	print(y)
+
+
+#mode
+print(max(b, key=b.get))
