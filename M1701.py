d9a263562318123dabfc382f2fe138b3ad57f8a6
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-01 10:15:22 +0800

    Update M1701.py

--- M1701.py
+++ M1701.py
@@ -1,21 +1,23 @@
 a = input().split(" ")
+a = [int(i) for i in a]
+
 cnt = 0
-if a[1] != "0":
-	cnt += int(a[1])
-if a[2] != "0":
-	cnt += int(a[2])
-if a[0] == "0":
-	if a[3] != "0":
-		cnt += int(a[3])*2
+if a[1] != 0:
+	cnt += a[1]
+if a[2] != 0:
+	cnt += a[2]
+if a[0] == 0:
+	if a[3] != 0:
+		cnt += a[3]*2
 else:
-	if a[3] == "0":
-		cnt += int(int(a[0])/2)
-		if int(a[0]) % 2 != 0:
+	if a[3] == 0:
+		cnt += int(a[0]/2)
+		if a[0] % 2 != 0:
 			cnt += 1
 	else:
-		cnt += int(a[3])
-		if int(a[3]) < int(a[0]):
-			z = int(a[0]) - int(a[3])
+		cnt += a[3]*2
+		if a[3] < a[0]*2:
+			z = a[0] - a[3]*2
 			cnt += int(z/2)
 			if z % 2 != 0:
 				cnt += 1
