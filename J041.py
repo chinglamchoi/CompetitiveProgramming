cf9cba217b34c219d4189a79e155f8211ccd44cd
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-01 10:14:33 +0800

    Update J041.py

--- J041.py
+++ J041.py
@@ -2,14 +2,13 @@ from math import gcd
 
 a = int(input())
 d = []
-b,c = input().split(" ")
-lcm = int(b) + int(c)
 
-for i in range(a-1):
+for i in range(a):
 	b,c = input().split(" ")
 	d.append(int(b)+int(c))
+lcm = d[0]
 
-for i in range(1, a-1):
-	lcm = lcm*d[i]//gcd(lcm, d[i])
+for i in d[1:]:
+	lcm = lcm*i//gcd(lcm, i)
 
 print(lcm)
