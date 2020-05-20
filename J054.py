3b807706d6b7567d2e5d837877c7deb3d7be0e3a
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-20 09:22:13 +0800

    Create J054.py

---
+++ J054.py
@@ -0,0 +1,33 @@
+# a,b = input().split()
+# a,b = int(a), int(b)
+# c = list(input())
+
+num = int(input())
+
+a = input().split()
+b = input().split()
+a = sorted([int(i) for i in a])
+b = sorted([int(i) for i in b])
+
+if a[0] > b[-1]:
+	print(num)
+elif a[-1] < b[0]:
+	print("0")
+else:
+	wins = 0
+	cnt = 0
+	valid = True
+	for i in range(num):
+		valid = True
+		if cnt == num:
+			break
+		while (a[cnt] <= b[i]) and (cnt < num):
+			cnt += 1
+			if (cnt == num) and (a[cnt] <= b[i]):
+				valid = False
+		if valid == False:
+			break
+		else:
+			wins += 1
+		cnt += 1
+	print(wins)
