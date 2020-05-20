2e297f3176297e8403de45fa2c1a7348c11c1467
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-20 09:22:32 +0800

    Update J054.py

--- J054.py
+++ J054.py
@@ -1,33 +1,36 @@
-# a,b = input().split()
-# a,b = int(a), int(b)
-# c = list(input())
-
 num = int(input())
-
-a = input().split()
-b = input().split()
-a = sorted([int(i) for i in a])
-b = sorted([int(i) for i in b])
-
-if a[0] > b[-1]:
-	print(num)
-elif a[-1] < b[0]:
-	print("0")
+if num == 1:
+	a = input()
+	b = input()
+	a,b = int(a), int(b)
+	if a <= b:
+		print("0")
+	else:
+		print("1")
 else:
-	wins = 0
-	cnt = 0
-	valid = True
-	for i in range(num):
+	a = input().split()
+	b = input().split()
+	a = sorted([int(i) for i in a])
+	b = sorted([int(i) for i in b])
+	if a[0] > b[-1]:
+		print(num)
+	elif a[-1] < b[0]:
+		print("0")
+	else:
+		wins = 0
+		cnt = 0
 		valid = True
-		if cnt == num:
-			break
-		while (a[cnt] <= b[i]) and (cnt < num):
+		for i in range(num):
+			valid = True
+			if cnt == num:
+				break
+			while (a[cnt] <= b[i]) and (cnt < num):
+				cnt += 1
+				if (cnt == num) and (a[cnt] <= b[i]):
+					valid = False
+			if valid == False:
+				break
+			else:
+				wins += 1
 			cnt += 1
-			if (cnt == num) and (a[cnt] <= b[i]):
-				valid = False
-		if valid == False:
-			break
-		else:
-			wins += 1
-		cnt += 1
-	print(wins)
+		print(wins)
