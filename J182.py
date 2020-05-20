e8a19cbe7c65377539132a9217dabeae5ab3c73a
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-20 09:24:06 +0800

    Update J182.py

--- J182.py
+++ J182.py
@@ -3,27 +3,25 @@ a,b,c = int(a), int(b), int(c)
 grid = [[0]*b for i in range(a)]
 for i in range(c):
 	x,y = input().split()
-	x,y = int(x), int(y)
-	grid[x-1][y-1] = 1
+	grid[int(x)-1][int(y)-1] = 1
 cnt, ropes, ropey, valid = 0, [], dict(), False
-
+u = b - 1
 for i in range(a):
 	if i & 1:
 		count = -1
-		for o in range(b):
-			if grid[i][count] == 0:
+		for o in range(u, -1, -1):
+			if grid[i][o] == 0:
 				if valid == True:
 					ropes[cnt-1] += 1
-					ropey[cnt].append(str(i+1) + " " + str(abs(o-b)))
+					ropey[cnt].append(str(i+1) + " " + str(o+1))
 				else:
 					cnt += 1
 					ropes.append(1)
-					ropey[cnt] = [str(i+1) + " " + str(abs(o-b))] #maybe x-1 faster?
+					ropey[cnt] = [str(i+1) + " " + str(o+1)]
 					valid = True
 			else:
 				if valid == True:
 					valid = False
-			count -= 1
 	else:
 		for o in range(b):
 			if grid[i][o] == 0:
@@ -39,7 +37,6 @@ for i in range(a):
 				if valid == True:
 					valid = False
 print(cnt)
-
 for i in range(cnt):
 	v = ropes[i]
 	print(v)
