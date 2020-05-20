20008274a17cfbd417189faca1b035c9b8629a51
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-20 09:23:52 +0800

    Update J182.py

--- J182.py
+++ J182.py
@@ -6,8 +6,25 @@ for i in range(c):
 	x,y = int(x), int(y)
 	grid[x-1][y-1] = 1
 cnt, ropes, ropey, valid = 0, [], dict(), False
+
 for i in range(a):
-	if i % 2 == 0:
+	if i & 1:
+		count = -1
+		for o in range(b):
+			if grid[i][count] == 0:
+				if valid == True:
+					ropes[cnt-1] += 1
+					ropey[cnt].append(str(i+1) + " " + str(abs(o-b)))
+				else:
+					cnt += 1
+					ropes.append(1)
+					ropey[cnt] = [str(i+1) + " " + str(abs(o-b))] #maybe x-1 faster?
+					valid = True
+			else:
+				if valid == True:
+					valid = False
+			count -= 1
+	else:
 		for o in range(b):
 			if grid[i][o] == 0:
 				if valid == True:
@@ -21,24 +38,11 @@ for i in range(a):
 			else:
 				if valid == True:
 					valid = False
-	else:
-		count = -1
-		for o in range(b):
-			if grid[i][count] == 0:
-				if valid == True:
-					ropes[cnt-1] += 1
-					ropey[cnt].append(str(i+1) + " " + str(abs(o-b)))
-				else:
-					cnt += 1
-					ropes.append(1)
-					ropey[cnt] = [str(i+1) + " " + str(abs(o-b))]
-					valid = True
-			else:
-				if valid == True:
-					valid = False
-			count -= 1
 print(cnt)
+
 for i in range(cnt):
-	print(ropes[i])
-	for o in range(ropes[i]):
-		print(ropey[i+1][o])
+	v = ropes[i]
+	print(v)
+	m = i + 1
+	for o in range(v):
+		print(ropey[m][o])
