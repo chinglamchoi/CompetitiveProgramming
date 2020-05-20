a8d6cfd12abad3ea61e366eb2a0ea7d5722e397d
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-21 00:09:25 +0800

    Update M19B1.cpp

--- M19B1.cpp
+++ M19B1.cpp
@@ -2,7 +2,6 @@
 #include <random>
 #include <types.h>
 #include <game_util.h>
-#pragma GCC optimize("Ofast")
 #include<bits/stdc++.h>
 using namespace std;
 using namespace game;
@@ -26,65 +25,13 @@ public:
   int sz[21] = {1, 2, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
   int dx[4] = {1, 1, -1, -1};
   int dy[4] = {-1, -1, 1, 1};
-  int getUsedPiece () {
-    PieceList used = current_game->used_pieces().first;
-    int res = 0;
-    for (int i = 0; i < 21; ++i)
-      if (!used[i]) res++;
-    return res;
-  }
-  int getSparse () {
-    Board x = current_game->board();
-    int res = 0;
-    for (int i = 0; i < 14; ++i)
-      for (int j = 0; j< 14; ++j)
-        if (!x[i][j]) res++;
-    return res;
-  }
-  ll WeightBeg (Board x, PieceList used) {
-   vector<Move> t = game_util::GetValidMoves(x, player, used);
-    int len = (int) t.size();
-    ll res = 0;
-    for (int i = 0; i < len; ++i) {
-      int id = t[i].piece().id() / 8;
-      res = res + sz[id] * sz[id] * sz[id] * 1000;
-    }
-    // cout << "done" << endl;
-    int cnt = 0, cnt2 = 0;
-    for (int i = 0; i < 14; ++i) {
-      for (int j = 0; j < 14; ++j) {
-        if (x[i][j] == player) {
-          for (int k = 0; k < 4; ++k) {
-           int nx = i + dx[k], ny = j + dy[k]; 
-           if (nx < 0 || nx >= 14 || ny < 0 || ny >= 14) continue;
-           if (!x[nx][ny]) cnt++;
-          }
-        } else if (x[i][j] == opponent) {
-          for (int k = 0; k < 4; ++k) {
-           int nx = i + dx[k], ny = j + dy[k]; 
-           if (nx < 0 || nx >= 14 || ny < 0 || ny >= 14) continue;
-           if (!x[nx][ny]) cnt++;
-          }
-        }
-      }
-    }
-    res = res + cnt * cnt * cnt * 1000 - cnt2 * cnt2 * cnt2 * 500;
-    int opponent = (player == 1 ? 2 : 1);
-    t = game_util::GetValidMoves(x, opponent, used);
-    len = (int) t.size();
-    for (int i = 0; i < len; ++i) {
-      int id = t[i].piece().id() / 8;
-      res = res - sz[id] * sz[id] * sz[id] * 500;
-    }
-    return res;
-  }
-  ll WeightMid (Board x, PieceList used) {
+  ll getWeight (Board x, PieceList used) {
     vector<Move> t = game_util::GetValidMoves(x, player, used);
     int len = (int) t.size();
     ll res = 0;
     for (int i = 0; i < len; ++i) {
       int id = t[i].piece().id() / 8;
-      res = res + sz[id] * sz[id] * sz[id] * 500;
+      res = res + sz[id] * sz[id] * 100;
     }
     // cout << "done" << endl;
     int cnt = 0, cnt2 = 0;
@@ -105,83 +52,35 @@ public:
         }
       }
     }
-    res = res + cnt * cnt * cnt * 500 - cnt2 * cnt2 * cnt2 * 1000;
+    res = res + cnt * cnt - cnt2 * cnt2 * cnt2;
     int opponent = (player == 1 ? 2 : 1);
     t = game_util::GetValidMoves(x, opponent, used);
     len = (int) t.size();
     for (int i = 0; i < len; ++i) {
       int id = t[i].piece().id() / 8;
-      res = res - sz[id] * sz[id] * sz[id] * 1000;
+      res = res - sz[id] * sz[id] * sz[id] * 100;
     }
     return res;
   }
-  ll WeightEn(Board x, PieceList used, int id) {
-    vector<Move> t = game_util::GetValidMoves(x, player, used);
-    int len = (int) t.size();
-    ll res = 0;
-    for (int i = 0; i < len; ++i) {
-      int id = t[i].piece().id() / 8;
-      res = res + sz[id] * sz[id] * sz[id] * 50;
-    }
-    // cout << "done" << endl;
-    int cnt = 0, cnt2 = 0;
-    for (int i = 0; i < 14; ++i) {
-      for (int j = 0; j < 14; ++j) {
-        if (x[i][j] == player) {
-          for (int k = 0; k < 4; ++k) {
-           int nx = i + dx[k], ny = j + dy[k]; 
-           if (nx < 0 || nx >= 14 || ny < 0 || ny >= 14) continue;
-           if (!x[nx][ny]) cnt++;
-          }
-        } else if (x[i][j] == opponent) {
-          for (int k = 0; k < 4; ++k) {
-           int nx = i + dx[k], ny = j + dy[k]; 
-           if (nx < 0 || nx >= 14 || ny < 0 || ny >= 14) continue;
-           if (!x[nx][ny]) cnt++;
-          }
-        }
-      }
-    }
-    res = res + cnt * cnt * cnt * 50 - cnt2 * cnt2 * cnt2 * 50;
-    int opponent = (player == 1 ? 2 : 1);
-    t = game_util::GetValidMoves(x, opponent, used);
-    len = (int) t.size();
-    for (int i = 0; i < len; ++i) {
-      int id = t[i].piece().id() / 8;
-      res = res - sz[id] * sz[id] * sz[id] * 50;
-    }
-    res += sz[id] * 100000;
-    return res;
-
-  }
-  ll getWeight (Board x, PieceList used, int id) {
-    ll sparse = getSparse();
-    if (sparse > 176) return WeightBeg(x, used);
-    else if (sparse > 70) return WeightMid(x, used);
-    else return WeightEn(x, used, id);
-    
-  }
-
   Move move(const vector<Move>& valid_moves) {
-    
     int len = valid_moves.size();
     Board current = current_game->board();
     ll mx = LLONG_MIN;
-    int idx = -1;
     assert(len != 0);
     int tot = 0;
     for (int i = 0; i < len; ++i) {
         Board tmp = game_util::ApplyMove(current, valid_moves[i]);
         PieceList used = (current_game->used_pieces()).first;
-        int id = valid_moves[i].piece().id() / 8;
         used[valid_moves[i].piece().id() / 8] = true;
-        ll weight = getWeight(tmp, used, id);
+        ll weight = getWeight(tmp, used);
         if (weight > mx) {
           mx = weight;
-          idx = i;
+          tot = 0;
         }
+        if (weight == mx) arr[++tot] = i;
     }
-    return valid_moves[idx];
+    uniform_int_distribution<int> d(1, tot);
+    return valid_moves[arr[d(g)]];
   }
 };
 
