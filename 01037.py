fd7948498aa0ff55b9ddbcd9bc13dc7e15ffb677
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-20 09:21:49 +0800

    Update 01037.py

--- 01037.py
+++ 01037.py
@@ -1,17 +1,14 @@
 import itertools as it
 a,b,c = input().split()
 a,b,c = int(a), int(b), int(c)
-d = list(input())
-d.sort()
-d = "".join(d)
+d = "".join(sorted(list(input())))
 prin = list(it.combinations(d, b))
-foo = ["@"]
+count = []
 cnt = 0
 for i in prin:
 	if cnt == c:
 		break
-	if i != foo[-1]:
-	#check if triggers index error
+	if i not in count:
 		print("".join(i))
-		foo.append(i)
+		count.append(i)
 		cnt += 1
