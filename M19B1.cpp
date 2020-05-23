13a93ff99e20b09f227dfcf7c34a7843e0b75c50
Ching Lam Choi <ccl5a09@gmail.com>
2020-05-24 00:13:10 +0800

    Update M19B1.cpp

--- M19B1.cpp
+++ M19B1.cpp
@@ -151,35 +151,7 @@ public:
           mx3=LLONG_MIN;
           tot = 0;
         }
-        if (weight == mx && weight2 == mx2 && sz[id] > mx3) {
-          mx3 = sz[id];
-          tot = 0;
-        }
-        if (weight == mx && weight2 == mx2 && sz[id] == mx3) arr[++tot] = i;
-    }
-    uniform_int_distribution<int> d(1, tot);
-    return valid_moves[arr[d(g)]];
-  }
-  Move moveOrange (const vector<Move>& valid_moves) {
-    otot++;
-    int tot = 0;
-    Board x = current_game->board();
-    PieceList purple, orange;
-    tie(purple, orange) = current_game->used_pieces();
-    ll mx = LLONG_MIN;
-    int len = (int) valid_moves.size(); 
-    for (int i = 0; i < len; ++i) {
-      Move m = valid_moves[i];
-      int id = m.piece().name();
-      if (otot <= 3 && sz[id] < 5) continue;
-      Board x2 = game_util::ApplyMove(x, m);
-        orange[id] = 1;
-        int t1 = (int) game_util::GetValidMoves(x2, 1, purple).size();
-        int t2 = (int) game_util::GetValidMoves(x2, 2, orange).size();
-        int dif = t2 - t1;
-        if (dif > mx) mx = dif, tot = 0;
-        if (dif == mx) arr[++tot] = i;
-        orange[id] = 0;
+        if (weight == mx && weight2 == mx2) arr[++tot] = i;
     }
     uniform_int_distribution<int> d(1, tot);
     return valid_moves[arr[d(g)]];
