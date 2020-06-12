46393b81648c58554f9bbbf34dd9e12271aff2f7
Ching Lam Choi <ccl5a09@gmail.com>
2020-06-12 13:03:54 +0800

    Update D102.java

--- D102.java
+++ D102.java
@@ -1,11 +1,9 @@
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 public class program {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         float a = Float.parseFloat(s.nextLine().substring(1));
-        DecimalFormat df = new DecimalFormat("#.#");
-        System.out.println("$" + df.format(a/2));
+        System.out.println("$" + Math.ceil(a*5)/10);
     }
 }
