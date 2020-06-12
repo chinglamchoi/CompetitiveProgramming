c4217bd3108b46c6c0cee303a4ce72d0c1ebe2cd
Ching Lam Choi <ccl5a09@gmail.com>
2020-06-12 13:02:55 +0800

    Create D100.java

---
+++ D100.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class program {
+	public static void main(String[] args) {
+		Scanner a = new Scanner(System.in);
+		int b = a.nextInt();
+		int c = a.nextInt();
+		a.close();
+		System.out.println(b + c);
+	}
+}
