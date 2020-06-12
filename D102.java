e90310de262df1f28d6ec34ae069793dc79e58a5
Ching Lam Choi <ccl5a09@gmail.com>
2020-06-12 13:03:25 +0800

    Create D102.java

---
+++ D102.java
@@ -0,0 +1,11 @@
+import java.utils.Scanner;
+import java.text.DecimalFormat;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        a = Float.parseFloat(s.nextLine().substring(1));
+        DecimalFormat df = new DecimalFormat("#.#");
+        System.out.println("$" + df.format(a/2));
+    }
+}
