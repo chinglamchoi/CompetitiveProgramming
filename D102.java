263a7061baf23145e4290d18a9276216ceea4493
Ching Lam Choi <ccl5a09@gmail.com>
2020-06-12 13:03:47 +0800

    Update D102.java

--- D102.java
+++ D102.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 public class program {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        a = Float.parseFloat(s.nextLine().substring(1));
+        float a = Float.parseFloat(s.nextLine().substring(1));
         DecimalFormat df = new DecimalFormat("#.#");
         System.out.println("$" + df.format(a/2));
     }
